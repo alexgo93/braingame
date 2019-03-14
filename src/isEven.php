37e8fa1e5f949e00a1c715819f3d024fd8592563
Alex <alekseimell5644@gmail.com>
2019-03-14 14:20:42 +0300

    even game add

---
+++ src/isEven.php
@@ -0,0 +1,39 @@
+<?php
+
+use function \cli\out;
+use function \cli\input;
+
+function isEvenGame()
+{
+    $digit = rand(1, 100);
+    static $counter = 0;
+
+    while($counter < 3) {
+        out("Is " . $digit . " Even?\n");
+        $answer = input();
+        if (isEven($digit) && $answer === 'yes') {
+            $counter++;
+            return isEvenGame();
+        } else if(!isEven($digit) && $answer === 'no'){
+            $counter++;
+            return isEvenGame();
+        } else {
+            $counter = 0;
+            out("Your loose! Want to try again? \n");
+            $looseInput = input();
+            if($looseInput === 'yes') {
+                return isEvenGame();
+            }
+            return false;
+        }
+    }
+    out("Congratulation, you win!\n");
+    return true;
+}
+
+
+
+function isEven($digit)
+{
+    return $digit % 2 == 0;
+}
